7205b6a5d5bd5e6669cbaa24bac600c6a765e89e
ezgatrabajo <ezgatrabajo@gmail.com>
2020-06-15 15:06:06 -0300

    service producto

--- src/main/java/com/cursoudemy/springbootservicesproductos/rest/ProductoRestController.java
+++ src/main/java/com/cursoudemy/springbootservicesproductos/rest/ProductoRestController.java
@@ -1,8 +1,33 @@
 package com.cursoudemy.springbootservicesproductos.rest;
 
+import com.cursoudemy.springbootservicesproductos.models.entity.Producto;
+import com.cursoudemy.springbootservicesproductos.models.service.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
+@RequestMapping("api/productos")
 @RestController
 public class ProductoRestController {
     
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping("/")
+    public List<Producto> listar(){
+        return productoService.findAll();
+    }
+
+    @GetMapping("/{id}/")
+    public Producto detalle(@PathVariable Long id){
+        return productoService.findById(id);
+    }
+
+
+
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into productos ( nombre, precio, create_at)values( 'producto 1', 250, now());
+insert into productos ( nombre, precio, create_at)values( 'producto 2', 500, now());
